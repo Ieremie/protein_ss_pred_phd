a298ec6a0cd70ac3f3c22e211d4bad208ede6f82
I.Ieremie <ieremie.ioan99@yahoo.com>
2020-11-14 11:49:21 +0000

    changed sbatch code, now is running fine

--- protein_prediction.sh
+++ protein_prediction.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu
-#SBATCH --time=03:00:00          # walltime
-
+#SBATCH --partition=gtx1080
+#SBATCH --gres=gpu:4
+#SBATCH --time=04:00:00          # walltime
+#SBATCH --mem=45G
 
 # mail alert at start, end and abortion of execution
 #SBATCH --mail-type=ALL
@@ -10,6 +11,12 @@
 # send mail to this address
 #SBATCH --mail-user=ii1g17@soton.ac.uk
 
-cd $HOME
+
+module load conda
+source activate gpu-iridis
+
+
+cd $HOME/protein_ss_pred_phd
+
 
 python protein_prediction.py
